0bb9c4f5508ce529b515a6cefd177e7545f59f4d
Magna011 <129706360+Magna011@users.noreply.github.com>
2023-08-03 12:52:25 +0700

    2 files modified, 1 added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import './App.css';
 import NavigationBar from './komponents/NavigationBar/NavigationBar'
+import Footer from './komponents/Footer/Footer'
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
+      <Footer/>
     </div>
   );
 }
--- src/komponents/NavigationBar/NavigationBar.jsx
+++ src/komponents/NavigationBar/NavigationBar.jsx
@@ -1,48 +1,39 @@
-import React, { useState } from 'react';
+// src/components/Navbar.jsx
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="padding: 20px,
-    text-align: center,
-    position: absolute,
-    left: 0,
-    bottom: 0,
-    width: 100%" >
-    <footer className="footer mt-100 p-10  bg-neutral text-neutral-content">
-      <div>
-        <span className="footer-title">NusaLearinng</span>
-        <a className="link link-hover">2023 NusaLearning</a>
-        <a className="link link-hover">All rights reversed</a>
-        <a className="link link-hover">PT. Solusi Teknologi Nusantara</a>
+    <nav className="bg-blue-500 p-4 flex justify-between items-center">
+      <div className="flex items-center space-x-4">
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fappadvice.com%2Fapp%2Fnusalms%2F1599993151&psig=AOvVaw3pGU068vBW3vSwtEp6x39r&ust=1691127628802000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIDbq7_jv4ADFQAAAAAdAAAAABAE" alt="Logo Brand" className="w-8 h-8" />
+        <input
+          type="text"
+          placeholder="Cari sesuatu..."
+          className="px-2 py-1 bg-white rounded-md focus:outline-none"
+        />
       </div>
-      <div>
-        <span className="footer-title">About</span>
-        <a className="link link-hover">Privacy Policy</a>
-        <a className="link link-hover">Help Center</a>
-        <a className="link link-hover">Terms & Condition</a>
+      <div></div>
+      <div className="flex items-center space-x-4">
+        <a href="#" className="text-white">
+          Beranda
+        </a>
+        <a href="#" className="text-white">
+          Pelatihan
+        </a>
+        <a href="#" className="text-white">
+          ID V
+        </a>
+        {/* Logo brand di sini */}
+        <img
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fid%2Fsearch%3Fq%3Dnotifikasi&psig=AOvVaw0jN2L4Y-N042-QP-iNh9SN&ust=1691127670526000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNj-n9Pjv4ADFQAAAAAdAAAAABAE"
+          alt="Notifikasi"
+          className="w-6 h-6"
+        />
+        <a href="#" className="text-white">
+          Login
+        </a>
       </div>
-      <div>
-        <span className="footer-title">Services</span>
-        <a className="link link-hover">Check Certificate </a>
-      </div>
-      <div>
-        <span className="footer-title">Follow Us on</span>
-        <div className="grid grid-flow-col gap-4">
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-       </div>
-      </div>
-      <div>
-      <span className="footer-title">Download NusaLearning Mobile</span>
-      <div>
-        <a href="" className="btn bg-white text-black ">Play Store</a>
-        <a> or </a>
-        <a href="" className="btn bg-white text-black">App Store</a>
-      </div>
-      </div>
-    </footer>
-    </div>
+    </nav>
   );
 };
 
